cf085c1f3df8f2a65e0cff42d95c724d3719bed6
almonsour13 <almonsoursalida@gmail.com>
2026-04-13 23:38:30 +0800

    refactor: uncomment two components

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,14 +1,9 @@
 import { ColumnView } from "@/shared/components/ui/custom-view";
-import * as BackgroundTask from "expo-background-task";
 import { ScrollView } from "react-native";
 import Header from "./components/header";
+import WeekDaysProgress from "./components/week-day-progress";
+import TodayActivities from "./components/today-activities";
 export default function HomeScreen() {
-    const triggerManualTask = async () => {
-        console.log("Manually triggering background worker...");
-        // This runs all registered tasks immediately in development mode
-        await BackgroundTask.triggerTaskWorkerForTestingAsync();
-    };
-
     return (
         <ScrollView
             showsHorizontalScrollIndicator={false}
@@ -16,9 +11,8 @@ export default function HomeScreen() {
         >
             <ColumnView className="pb-28 gap-8">
                 <Header />
-                {/* <Calendar /> */}
-                {/* <WeekDaysProgress />
-                <TodayActivities /> */}
+                <WeekDaysProgress />
+                <TodayActivities />
             </ColumnView>
         </ScrollView>
     );
